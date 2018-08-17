234e9019959d4c18fdf11257f20d146ebb4143e2
TP <prasol@o2.pl>
2018-08-17 08:23:56 +0200

    Add Salt property

--- User.cs
+++ User.cs
@@ -4,4 +4,5 @@ public class User
     public string LastName { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
+    public string Salt { get; set; }
 }
