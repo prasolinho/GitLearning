767b37b6b9eca80a03e52b67f2622312373c391b
TP <prasol@o2.pl>
2018-08-17 08:22:05 +0200

    Add User class

---
+++ User.cs
@@ -0,0 +1,7 @@
+public class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
