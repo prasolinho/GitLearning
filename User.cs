dbb2e63311b19afba4619c5979ecd480eb025953
TP <prasol@o2.pl>
2018-08-18 06:03:43 +0200

    User.cs - remove first and last name, add Id property

--- User.cs
+++ User.cs
@@ -1,7 +1,6 @@
 public class User
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public int Id { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
     public string Salt { get; set; }
